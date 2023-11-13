cde3319d42251fb3da7cd7043894735c8cf4148d
emmet hamell <emmethamell@yahoo.com>
2023-11-13 02:04:04 -0500

    commit

--- View/MainView/MainView.swift
+++ View/MainView/MainView.swift
@@ -8,29 +8,14 @@
 import SwiftUI
 
 struct MainView: View {
-
     var body: some View {
-        
-        TabView{
-            Text("Friend activities")
-                .tabItem {
-                    Image(systemName: "person.3")
-                    Text("Friends")
-                }
-            
-            InviteView()
-                .tabItem {
-                    Image(systemName: "hand.wave.fill")
-                    Text("Invite")
-                }
-
-            
-        }
+        //invite page
+        Text("main view")
     }
 }
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
-        MainView()
+        ContentView()
     }
 }
