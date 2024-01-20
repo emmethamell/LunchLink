26f938fc625b7c5a26e4f18be00fa202f3c9914c
emmet hamell <emmethamell@yahoo.com>
2024-01-20 11:09:04 -0500

    use sdwebimage to load profile pic

--- View/LikedUsers/LikedUsersView.swift
+++ View/LikedUsers/LikedUsersView.swift
@@ -5,6 +5,7 @@
 //  Created by Emmet Hamell on 12/22/23.
 //
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct LikedUsersView: View {
     
@@ -14,37 +15,29 @@ struct LikedUsersView: View {
     
     @AppStorage("first_name") private var firstName = ""
     @AppStorage("last_name") private var lastName = ""
-    
+    @State private var isImageLoaded = false
     var body: some View {
-        NavigationView {
+        NavigationStack {
             List(viewModel.users) { user in
                 NavigationLink(destination: ReusableProfileContent(user: user, userUID: currentUserUID, firstName: firstName, lastName: lastName)) {
                     HStack {
-                        AsyncImage(url: user.userProfileURL) { phase in
-                            switch phase {
-                            case .success(let image):
-                                image.resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(width: 50, height: 50)
-                                    .clipShape(Circle())
-                            case .failure:
-                                Image(systemName: "person.crop.circle.badge.xmark")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(width: 50, height: 50)
-                                    .clipShape(Circle())
-                            case .empty:
-                                ProgressView()
-                            @unknown default:
-                                EmptyView()
+                        WebImage(url: user.userProfileURL)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 50, height: 50)
+                            .clipShape(Circle())
+                            .onAppear(){
+                                self.isImageLoaded = true
                             }
-                        }
+                        
                         
                         Text(user.username)
                             .font(.body)
                             .padding(.leading, 10)
                     }
                 }
+                .disabled(!isImageLoaded)
+                
             }
             .onAppear {
                 Task {
